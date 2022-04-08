0eb22154da14e68bb4000a023c24d9ba8f97040d
Akanksha Singh <singhakanksha001@gmail.com>
2022-04-07 23:31:27 -0400

    destructuring variables

--- src/Components/ImageList.js
+++ src/Components/ImageList.js
@@ -1,10 +1,8 @@
 import React from 'react';
 
 const ImageList = props => {
-  const img = props.images.map(image => {
-    return (
-      <img key={image.id} src={image.urls.regular} alt={image.description} />
-    );
+  const img = props.images.map(({ description, id, urls }) => {
+    return <img key={id} src={urls.regular} alt={description} />;
   });
   return <div>{img}</div>;
 };
